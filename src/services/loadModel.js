4528bc6944987d6603ccc96de2f3017b42d39000
rznafs <160302437+rznafs@users.noreply.github.com>
2024-12-08 13:30:08 +0700

    Delete src directory

--- src/services/loadModel.js
+++
@@ -1,5 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/model-cp-storage/model.json');
-}
-module.exports = loadModel;
